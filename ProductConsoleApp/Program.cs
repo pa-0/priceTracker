52ad01adb8fddb774dcab61d35b44978e0f6d84d
itu-itis21-ahmed20 <ahmed20@itu.edu.tr>
2024-07-02 12:45:21 +0300

    add consoleApp

---
+++ ProductConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using priceTracker;
+using priceTracker.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace ProductConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<MyDbcontext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+            using var dbContext = new MyDbcontext(optionsBuilder.Options);
+
+            var productsController = new ProductsController(dbContext);
+
+            var entries = dbContext.Entrys.ToList();
+
+            foreach (var entry in entries)
+            {
+                var result = productsController.CreateProductFromEntry(entry.Id);
+
+                    Console.WriteLine($"Products created for entry ID {entry.Id}");
+                
+            }
+        }
+    }
+}
